2f19502e2b5a78daf03dc47ef382358b7d617c73
kube-jay <dwana203@gmail.com>
2024-12-30 23:20:55 +0300

    All classes related to the project

---
+++ src/University.java
@@ -0,0 +1,59 @@
+public class University {
+
+	public String name;
+	public String address;
+	public char phone;
+
+	public void setPhone(char phone) {
+		this.phone = phone;
+	}
+
+	public void toProvideEducation() {
+		// TODO - implement University.toProvideEducation
+		throw new UnsupportedOperationException();
+	}
+
+	public void addEmployee() {
+		// TODO - implement University.addEmployee
+		throw new UnsupportedOperationException();
+	}
+
+	public String getPhone() {
+		// TODO - implement University.getPhone
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param phone
+	 */
+	public void setPhone(String phone) {
+		// TODO - implement University.setPhone
+		throw new UnsupportedOperationException();
+	}
+
+	public String getAddress() {
+		return this.address;
+	}
+
+	/**
+	 * 
+	 * @param address
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
